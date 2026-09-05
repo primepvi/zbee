b280c0bc523f3f22afe78af8dc34a6d4b6fe09ac
primepvi <pedrobernardesv@gmail.com>
2026-09-05 14:17:24 -0300

    feat(TypeChecker): add type checking for expressions.

--- src/TypeChecker.zig
+++ src/TypeChecker.zig
@@ -9,20 +9,23 @@ const symbols_mod = @import("internals/symbols.zig");
 const SymbolTable = symbols_mod.SymbolTable;
 const Symbol = symbols_mod.Symbol;
 const Type = symbols_mod.Type;
+const TypeKind = symbols_mod.TypeKind;
 
 const Flow = struct {
     can_continue: bool,
 };
 
 pub const TypeChecker = struct {
+    allocator: std.mem.Allocator,
     ast: *const AST,
     symbols: SymbolTable,
     expected_return_type: Type,
 
     const Self = @This();
 
-    pub fn init(ast: *const AST) Self {
+    pub fn init(allocator: std.mem.Allocator, ast: *const AST) Self {
         return .{
+            .allocator = allocator,
             .ast = ast,
             .symbols = SymbolTable.init(.global),
             .expected_return_type = Type.invalid(),
@@ -35,8 +38,8 @@ pub const TypeChecker = struct {
         }
     }
 
-    fn checkStmt(self: *Self, stmt: *const Stmt) !Flow {
-        switch(stmt.*) {
+    fn checkStmt(self: *Self, stmt: *const Stmt) anyerror!Flow {
+        switch (stmt.*) {
             .echo_stmt => try self.checkEchoStmt(stmt),
             .expr_stmt => try self.checkExprStmt(stmt),
             .variable_decl_stmt => try self.checkVariableDeclStmt(stmt),
@@ -50,7 +53,7 @@ pub const TypeChecker = struct {
         }
     }
 
-    fn checkEchoStmt(self: *Self, stmt: *const Stmt) !Flow {
+    fn checkEchoStmt(self: *Self, stmt: *const Stmt) anyerror!Flow {
         const echo = stmt.echo_stmt;
         const message_type = try self.checkExpr(echo.message);
         if (message_type.isEmpty()) {
@@ -62,17 +65,17 @@ pub const TypeChecker = struct {
         };
     }
 
-    fn checkExprStmt(self: *Self, stmt: *const Stmt) !Flow {
+    fn checkExprStmt(self: *Self, stmt: *const Stmt) anyerror!Flow {
         _ = try self.checkExpr(stmt.expr_stmt.expr);
         return .{
             .can_continue = true,
         };
     }
 
-    fn checkVariableDeclStmt(self: *Self, stmt: *const Stmt) !Flow {
+    fn checkVariableDeclStmt(self: *Self, stmt: *const Stmt) anyerror!Flow {
         const decl = stmt.variable_decl_stmt;
         if (self.symbols.scopeHas(decl.identifier_token.lexeme)) {
-            // TODO: add variable already declared diagnostic. 
+            // TODO: add variable already declared diagnostic.
         }
 
         const value_type = try self.checkExpr(decl.value);
@@ -101,23 +104,23 @@ pub const TypeChecker = struct {
         };
 
         try self.symbols.put(symbol);
-        
+
         return .{
             .can_continue = true,
         };
     }
 
-    fn checkFunctionDeclStmt(self: *Self, stmt: *const Stmt) !Flow {
+    fn checkFunctionDeclStmt(self: *Self, stmt: *const Stmt) anyerror!Flow {
         const decl = stmt.function_decl_stmt;
         if (self.symbols.scope != .global) {
             // TODO: add invalid function decl diagnostic.
         }
 
-        if (self.symbols.scopeHas(decl.identifier_token.lexeme)){
+        if (self.symbols.scopeHas(decl.identifier_token.lexeme)) {
             // TODO: add function already declared diagnostic.
         }
 
-        var param_types = try std.ArrayList(Type).initCapacity(decl.params.capacity);
+        var param_types = try std.ArrayList(Type).initCapacity(self.allocator, decl.params.capacity);
         var scope = SymbolTable.init(.function, self.symbols);
         for (decl.params.items) |param| {
             const param_type = Type.fromAnnotation(param.type_annotation);
@@ -137,7 +140,8 @@ pub const TypeChecker = struct {
             scope.put(param_symbol);
         }
 
-        const return_type = Type.fromAnnotation(decl.type_annotation);
+        const return_type = self.allocator.create(Type);
+        return_type.* = Type.fromAnnotation(decl.type_annotation);
         if (return_type.isInvalid()) {
             // TODO: add invalid return type diagnostic.
         }
@@ -146,7 +150,7 @@ pub const TypeChecker = struct {
             .function = .{
                 .name = decl.identifier_token.lexeme,
                 .arity = decl.params.items.len,
-                .typing = return_type,
+                .typing = Type.function(param_types, return_type),
             },
         };
 
@@ -154,7 +158,7 @@ pub const TypeChecker = struct {
         scope.put(symbol);
 
         const prev_return_type = self.expected_return_type;
-        self.expected_return_type = return_type;
+        self.expected_return_type = return_type.*;
         self.symbols = &scope;
 
         const flow = try self.checkStmt(decl.body);
@@ -170,7 +174,7 @@ pub const TypeChecker = struct {
         };
     }
 
-    fn checkBlockStmt(self: *Self, stmt: *const Stmt) !Flow {
+    fn checkBlockStmt(self: *Self, stmt: *const Stmt) anyerror!Flow {
         const block = stmt.block_stmt;
         var scope = SymbolTable.init(.block, self.symbols);
         self.symbols = &scope;
@@ -184,20 +188,20 @@ pub const TypeChecker = struct {
         }
 
         self.symbols = scope.parent.?;
-        
+
         return flow;
     }
 
-    fn checkIfStmt(self: *Self, stmt: *const Stmt) !Flow {
+    fn checkIfStmt(self: *Self, stmt: *const Stmt) anyerror!Flow {
         const if_stmt = stmt.if_stmt;
         const condition_type = try self.checkExpr(if_stmt.condition);
         if (condition_type.kind != .bool_t) {
             // TODO: add invalid if condition diagnostic.
         }
-        
+
         const consequent_flow = try self.checkStmt(if_stmt.consequent);
         var alternate_flow: Flow = .{ .can_continue = true };
-          if (if_stmt.alternate) |alt| {
+        if (if_stmt.alternate) |alt| {
             alternate_flow = try self.checkStmt(alt);
         }
 
@@ -206,7 +210,7 @@ pub const TypeChecker = struct {
         };
     }
 
-    fn checkWhileStmt(self: *Self, stmt: *const Stmt) !Flow {
+    fn checkWhileStmt(self: *Self, stmt: *const Stmt) anyerror!Flow {
         const while_stmt = stmt.while_stmt;
         const condition_type = try self.checkExpr(while_stmt.condition);
         if (condition_type.kind != .bool_t) {
@@ -218,11 +222,11 @@ pub const TypeChecker = struct {
 
         const flow = try self.checkStmt(while_stmt.body);
         self.symbols = scope.parent.?;
-        
+
         return flow;
     }
 
-    fn checkForStmt(self: *Self, stmt: *const Stmt) !Flow {
+    fn checkForStmt(self: *Self, stmt: *const Stmt) anyerror!Flow {
         const for_stmt = stmt.for_stmt;
         const scope = SymbolTable.init(.block, self.symbols);
         self.symbols = &scope;
@@ -234,13 +238,13 @@ pub const TypeChecker = struct {
         }
 
         _ = try self.checkExpr(for_stmt.update);
-        
+
         const flow = try self.checkStmt(for_stmt.body);
         self.symbols = scope.parent.?;
         return flow;
     }
 
-    fn checkReturnStmt(self: *Self, stmt: *const Stmt) !Flow {
+    fn checkReturnStmt(self: *Self, stmt: *const Stmt) anyerror!Flow {
         const ret = stmt.return_stmt;
         var function_scope = self.symbols;
         while (function_scope.parent != null and function_scope.scope != .function) {
@@ -261,8 +265,8 @@ pub const TypeChecker = struct {
         };
     }
 
-    fn checkExpr(self: *Self, expr: *const Expr) !Type {
-        return switch(expr.*) {
+    fn checkExpr(self: *Self, expr: *const Expr) anyerror!Type {
+        return switch (expr.*) {
             .literal_expr => try self.checkLiteralExpr(expr),
             .identifier_expr => try self.checkIdentifierExpr(expr),
             .unary_expr => try self.checkUnaryExpr(expr),
@@ -273,5 +277,141 @@ pub const TypeChecker = struct {
             .call_expr => try self.checkCallExpr(expr),
             else => unreachable,
         };
+    }
+
+    fn checkLiteralExpr(_: *Self, expr: *const Expr) anyerror!Type {
+        const literal = expr.literal_expr;
+        return switch (literal.value_token.kind) {
+            .number_literal => Type.fromLexeme("int"),
+            .string_literal => Type.fromLexeme("string"),
+            .true_keyword, .false_keyword => Type.fromLexeme("bool"),
+            .null_keyword => Type.fromLexeme("null"),
+            else => unreachable,
+        };
+    }
+
+    fn checkIdentifierExpr(self: *Self, expr: *const Expr) anyerror!Type {
+        const ident = expr.identifier_expr;
+        const symbol = self.symbols.get(ident.identifier_token.lexeme);
+        if (symbol == null) {
+            // TODO: add undefined identifier diagnostic.
+        }
+
+        return symbol.?.getType();
+    }
+
+    fn checkUnaryExpr(self: *Self, expr: *const Expr) anyerror!Type {
+        const unary = expr.unary_expr;
+        const operand_type = try self.checkExpr(unary.operand);
+        if (!operand_type.supportsUnaryOperator(unary.operator_token.kind)) {
+            // TODO: add unsuported unary operation diagnostic.
+        }
+
+        return operand_type;
+    }
+
+    fn checkBinaryExpr(self: *Self, expr: *const Expr) anyerror!Type {
+        const binary = expr.binary_expr;
+        const left_type = try self.checkExpr(binary.left);
+        const right_type = try self.checkExpr(binary.right);
+        if (!left_type.supportsBinaryOperator(right_type, binary.operator_token.kind)) {
+            // TODO: add unsuported binary operation diagnostic.
+        }
+        
+        return switch (binary.operator_token.kind) {
+            .minus_symbol, .plus_symbol, .star_symbol, .slash_symbol, .percentage_symbol => Type.fromLexeme("int"),
+
+            .lt_symbol, .lte_symbol, .gt_symbol, .gte_symbol, .eqeq_symbol, .neq_symbol, .and_symbol, .or_symbol => Type.fromLexeme("bool"),
+            else => unreachable,
+        };
+    }
+
+    fn checkAssignmentExpr(self: *Self, expr: *const Expr) anyerror!Type {
+        const assignment = expr.assignment_expr;
+        const symbol = self.symbols.get(assignment.identifier_token.lexeme);
+        if (symbol == null) {
+            // TODO: add undefined identifier diagnostic.
+        }
+
+        if (!symbol.?.isVariable()) {
+            // TODO: add attempt to assign a non-variable identifier.
+        }
+
+        const variable = symbol.?.variable;
+        if (variable.constant) {
+            // TODO: add attempt to assign a constant variable.
+        }
+
+        const value_type = try self.checkExpr(assignment.value);
+        if (!value_type.isAssignableTo(variable.typing)) {
+            // TODO: add type mismatch diagnostic.
+        }
+
+        return value_type;
+    }
+
+    fn checkWhenExpr(self: *Self, expr: *const Expr) anyerror!Type {
+        const when = expr.when_expr;
+        const condition_type = try self.checkExpr(when.condition);
+        if (condition_type.kind != .bool_t) {
+            // TODO: add invalid when condition type.
+        }
+
+        const consequent_type = try self.checkExpr(when.consequent);
+        const alternate_type = try self.checkExpr(when.alternate);
+
+        var expected: Type = undefined;
+        var received: Type = undefined;
+        if (consequent_type.kind == .null_t) {
+            received = consequent_type;
+            expected = alternate_type;
+            expected.nullable = true;
+        } else if (alternate_type.kind == .null_t) {
+            received = alternate_type;
+            expected = consequent_type;
+            expected.nullable = true;
+        } else {
+            expected = consequent_type;
+            received = alternate_type;
+        }
+
+        if (std.mem.eql(TypeKind, expected.kind, received.kind) and alternate_type.nullable) {
+            expected.nullable = true;
+        }
+
+        if (!received.isAssignableTo(expected)) {
+            // TODO: add type mismatch diagnostic.
+        }
+
+        return expected;
+    }
+
+    fn checkCallExpr(self: *Self, expr: *const Expr) anyerror!Type {
+        const call = expr.call_expr;
+        const symbol = self.symbols.get(call.identifier_token.lexeme);
+        if (symbol == null) {
+            // TODO: add attempt to call a undefined function diagnostic.
+        }
+
+        if (!symbol.?.isFunction()) {
+            // TODO: add attempt to call a non-function diagnostic.
+        }
+
+        const function = symbol.?.function;
+        if (call.arguments.items.len != function.arity) {
+            // TODO: add invalid number of arguments diagnostic.
+        }
+
+        const param_types = function.typing.kind.function_t.param_types;
+        for (0..param_types.items.len) |i| {
+            const param_type = param_types.items[i];
+            const argument_type = try self.checkExpr(call.arguments.items[i]);
+            if (!argument_type.isAssignableTo(param_type)) {
+                // TODO: add type mismatch diagnostic.
+            }
+        }
+
+        const return_type = function.typing.kind.function_t.return_type;
+        return return_type;
     }
 };
--- src/internals/symbols.zig
+++ src/internals/symbols.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
+const TokenKind = @import("Token.zig").TokenKind;
 const TypeAnnotation = @import("ast.zig").TypeAnnotation;
 
 pub const FunctionType = struct {
     param_types: std.ArrayList(Type),
     return_type: *Type,
 };
-    
+
 pub const TypeKind = union(enum) {
     int_t,
     bool_t,
@@ -22,12 +23,13 @@ pub const Type = struct {
 
     const Self = @This();
     const typings_map = std.StaticStringMap(TypeKind).initComptime(.{
-        .{"int", .int_t},
-        .{"bool", .bool_t},
-        .{"string", .string_t},
-        .{"void", .void_t},
+        .{ "int", .int_t },
+        .{ "bool", .bool_t },
+        .{ "string", .string_t },
+        .{ "void", .void_t },
+        .{ "null", .null_t },
     });
-    
+
     pub fn fromLexeme(lexeme: []const u8) Self {
         return .{
             .kind = typings_map.get(lexeme) orelse .invalid_t,
@@ -39,32 +41,44 @@ pub const Type = struct {
         return .{
             .kind = typings_map.get(annotation.identifier_token.lexeme) orelse .invalid_t,
             .nullable = annotation.nullable,
-        };                     
+        };
+    }
+
+    pub fn function(params_types: std.ArrayList(Type), return_type: *Type) Self {
+        const function_type: FunctionType = .{
+            .param_types = params_types,
+            .return_type = return_type,
+        };
+
+        return .{
+            .kind = .{ .function_t = function_type },
+            .nullable = false,
+        };
     }
 
     pub fn invalid() Self {
         return .{
             .kind = .invalid_t,
             .nullable = false,
-        };    
+        };
     }
 
     pub fn empty() Self {
         return .{
             .kind = .void_t,
             .nullable = false,
-        };    
+        };
     }
 
     pub fn isEmpty(self: *Self) bool {
-        return switch(self.*) {
+        return switch (self.*) {
             .void_t => true,
             else => false,
         };
     }
 
     pub fn isInvalid(self: *Self) bool {
-        return switch(self.*) {
+        return switch (self.*) {
             .invalid_t => true,
             else => false,
         };
@@ -78,12 +92,28 @@ pub const Type = struct {
             return is_equal and !self.nullable;
         }
     }
+
+    pub fn supportsUnaryOperator(self: *const Self, operator: TokenKind) bool {
+        return switch (operator) {
+            .minus_symbol, .plus_symbol => self.kind == .int_t,
+            .not_symbol => self.kind == .bool_t,
+            else => false,
+        };
+    }
+
+    pub fn supportsBinaryOperator(self: *const Self, other: *const Self, operator: TokenKind) bool {
+        return switch (operator) {
+            .minus_symbol, .plus_symbol, .star_symbol, .slash_symbol, .percentage_symbol, .lt_symbol, .gt_symbol, .lte_symbol, .gte_symbol => self.kind == .int_t and other.kind == .int_t,
+
+            .eqeq_symbol, .neq_symbol => !(self.isEmpty() or self.isInvalid()) and !(other.isEmpty() or other.isInvalid())
+                .and_keyword,
+            .or_keyword => self.kind == .bool_t and other.kind == .bool_t,
+            else => false,
+        };
+    }
 };
 
-pub const SymbolKind = enum {
-    variable,
-    function
-};
+pub const SymbolKind = enum { variable, function };
 
 pub const VariableSymbol = struct {
     name: []const u8,
@@ -102,20 +132,39 @@ pub const Symbol = union(SymbolKind) {
     function: FunctionSymbol,
 
     const Self = @This();
-    
+
     pub fn getName(self: *const Self) []const u8 {
-        return switch(self.*) {
+        return switch (self.*) {
             .variable => |v| v.name,
             .function => |f| f.name,
+            else => unreachable,
+        };
+    }
+
+    pub fn getType(self: *const Self) Type {
+        return switch (self.*) {
+            .variable => |v| v.typing,
+            .function => |f| f.typing,
+            else => unreachable,
+        };
+    }
+
+    pub fn isVariable(self: *const Self) bool {
+        return switch (self.*) {
+            .variable => true,
+            else => false,
+        };
+    }
+
+    pub fn isFunction(self: *const Self) bool {
+        return switch (self.*) {
+            .function => true,
+            else => false,
         };
     }
 };
 
-pub const ScopeKind = enum {
-    global,
-    function,
-    block
-};
+pub const ScopeKind = enum { global, function, block };
 
 pub const SymbolTable = struct {
     scope: ScopeKind,
@@ -138,5 +187,15 @@ pub const SymbolTable = struct {
 
     pub fn put(self: *Self, symbol: Symbol) !void {
         try self.env.put(symbol.getName(), symbol);
+    }
+
+    pub fn get(self: *Self, name: []const u8) ?Symbol {
+        if (self.scopeHas(name)) {
+            return self.env.get(name);
+        } else if (self.parent != null) {
+            return self.parent.?.get(name);
+        } else {
+            return null;
+        }
     }
 };
